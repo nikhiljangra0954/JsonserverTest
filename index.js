474963d06c2477edcbfe6c1bea3d6e6358fe002d
NikJangra <nikhilkumar121288@gmail.com>
2023-04-21 12:24:36 +0530

    json-server

--- index.js
+++ index.js
@@ -11,59 +11,59 @@ server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
 // Register a new user
-server.post('/users/register', (req, res) => {
-const { email, password } = req.body
-if (!email || !password) {
-res.status(400).json({ error: 'Email and password are required' })
-return
-}
-// const salt = bcrypt.genSaltSync(10)
-// const hash = bcrypt.hashSync(password, salt)
-const user = {username, email, password }
-router.db.get('users').push(user).write()
-res.status(200).json({ message: 'User registered successfully' })
-})
+// server.post('/users/register', (req, res) => {
+// const { email, password } = req.body
+// if (!email || !password) {
+// res.status(400).json({ error: 'Email and password are required' })
+// return
+// }
+// // const salt = bcrypt.genSaltSync(10)
+// // const hash = bcrypt.hashSync(password, salt)
+// const user = {username, email, password }
+// router.db.get('users').push(user).write()
+// res.status(200).json({ message: 'User registered successfully' })
+// })
 
-// Login an existing user and get JWT token
-server.post('/users/login', (req, res) => {
-const { email, password } = req.body
-if (!email || !password) {
-res.status(400).json({ error: 'Email and password are required' })
-return
-}
-const user = router.db.get('users').find({ email }).value()
-console.log(user)
-if (!user) {
-res.status(401).json({ error: 'Invalid email or password' })
-return
-}
-// const match = bcrypt.compareSync(password, user.password)
-// console.log(match)
-// if (!match) {
+// // Login an existing user and get JWT token
+// server.post('/users/login', (req, res) => {
+// const { email, password } = req.body
+// if (!email || !password) {
+// res.status(400).json({ error: 'Email and password are required' })
+// return
+// }
+// const user = router.db.get('users').find({ email }).value()
+// console.log(user)
+// if (!user) {
 // res.status(401).json({ error: 'Invalid email or password' })
 // return
 // }
-const token = jwt.sign({ email }, 'secret')
-res.status(200).json({ message: 'User authenticated successfully', token })
-})
+// // const match = bcrypt.compareSync(password, user.password)
+// // console.log(match)
+// // if (!match) {
+// // res.status(401).json({ error: 'Invalid email or password' })
+// // return
+// // }
+// const token = jwt.sign({ email }, 'secret')
+// res.status(200).json({ message: 'User authenticated successfully', token })
+// })
 
-// Middleware to verify JWT token
-const verifyToken = (req, res, next) => {
-const authHeader = req.headers.authorization
-if (!authHeader) {
-res.status(401).json({ error: 'Authorization header is missing' })
-return
-}
-const token = authHeader.split(' ')[1]
-try {
-const decoded = jwt.verify(token, 'secret')
-req.user = decoded
-next()
-} catch (err) {
-res.status(401).json({ error: 'Invalid token' })
-return
-}
-}
+// // Middleware to verify JWT token
+// const verifyToken = (req, res, next) => {
+// const authHeader = req.headers.authorization
+// if (!authHeader) {
+// res.status(401).json({ error: 'Authorization header is missing' })
+// return
+// }
+// const token = authHeader.split(' ')[1]
+// try {
+// const decoded = jwt.verify(token, 'secret')
+// req.user = decoded
+// next()
+// } catch (err) {
+// res.status(401).json({ error: 'Invalid token' })
+// return
+// }
+// }
 const PORT = 8000
 
 server.listen(PORT, () => {
